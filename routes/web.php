f7636efca35d4dfe26aac98e6faa6df43e6e083e
Luis Felipe <108816416+luisfe1ipe@users.noreply.github.com>
2023-06-21 12:19:58 -0300

    navbar

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Home::class);
+Route::get('/', Home::class)->name('home');
 Route::get('/create-project', CreateProject::class)->name('create-project');
 Route::get('projects', IndexProjects::class)->name('index-projects');
 
